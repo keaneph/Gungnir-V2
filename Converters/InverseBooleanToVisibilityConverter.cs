f309665ea1511a34d159d60d4e050bd1816b2615
keaneph <nearpharelle@gmail.com>
2025-04-27 15:44:00 +0800

    chore/cleaned up folder structure

--- Converters/InverseBooleanToVisibilityConverter.cs
+++ Converters/InverseBooleanToVisibilityConverter.cs
@@ -3,7 +3,7 @@ using System.Globalization;
 using System.Windows;
 using System.Windows.Data;
 
-namespace sis_app
+namespace sis_app.Converters
 {
     public class InverseBooleanToVisibilityConverter : IValueConverter
     {
